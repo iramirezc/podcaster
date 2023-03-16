8f8eaf0afb1ab08e5c294c9e519bdbfb64bf667e
Isaac Ramirez <iramirezc@live.com.mx>
2023-03-16 22:34:41 +0100

    Add global styles.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,10 @@
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 import { theme } from '../theme/default-theme';
+import { GlobalStyles } from './global-styles';
 
 export const ThemeProvider = ({ children }: React.PropsWithChildren) => (
-  <StyledThemeProvider theme={theme}>{children}</StyledThemeProvider>
+  <StyledThemeProvider theme={theme}>
+    <GlobalStyles />
+    {children}
+  </StyledThemeProvider>
 );
