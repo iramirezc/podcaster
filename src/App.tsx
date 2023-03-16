7b83b778c6a73810972d0b419cade7511e564177
Isaac Ramirez <iramirezc@live.com.mx>
2023-03-16 22:54:16 +0100

    Implement AppHeader in App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { AppHeader } from 'components';
+import { Counter } from 'features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <AppHeader />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
