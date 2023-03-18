23459d5e9c03814d07df1e6c53b028c4e0eea355
Isaac Ramirez <iramirezc@live.com.mx>
2023-03-18 21:06:43 +0100

    Create basic structure for PodcastCard.

---
+++ src/features/podcasts/PodcastCard/PodcastCard.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const PodcastContainer = styled.article``;
+
+export const PodcastCover = styled.img``;
+
+export const PodcastTitle = styled.h2``;
+
+export const PodcastAuthor = styled.p``;
---
+++ src/features/podcasts/PodcastCard/PodcastCard.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import {
+  PodcastAuthor,
+  PodcastContainer,
+  PodcastCover,
+  PodcastTitle,
+} from './PodcastCard.styles';
+
+interface PodcastCardProps {
+  podcastId: string;
+  coverUrl: string;
+  title: string;
+  author: string;
+}
+
+export const PodcastCard: FC<PodcastCardProps> = ({
+  podcastId,
+  coverUrl,
+  title,
+  author,
+}) => (
+  <PodcastContainer id={`podcast-${podcastId}`}>
+    <PodcastCover src={coverUrl} />
+    <PodcastTitle>{title}</PodcastTitle>
+    <PodcastAuthor>{author}</PodcastAuthor>
+  </PodcastContainer>
+);
