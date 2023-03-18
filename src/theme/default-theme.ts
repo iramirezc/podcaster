5562b0850fcfeeb2986174700e9daba7cd99aa4c
Isaac Ramirez <iramirezc@live.com.mx>
2023-03-18 23:04:58 +0100

    Update shadow color

--- src/theme/default-theme.ts
+++ src/theme/default-theme.ts
@@ -8,7 +8,7 @@ export const theme: DefaultTheme = {
       primary: '#000000',
       secondary: '#808080',
     },
-    shadow: '0px 8px 16px -4px rgba(0,0,0,0.75)',
+    shadow: '0px 5px 10px -5px rgb(0 0 0 / 50%)',
   },
   typography: {
     htmlFontSize: '16px',
