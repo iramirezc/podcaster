cccced8b00889bfe4bc484158d9de2c5298ac0d6
Isaac Ramirez <iramirezc@live.com.mx>
2023-03-18 22:59:05 +0100

    Implement PodcastGrid.

---
+++ src/features/podcasts/PodcastGrid/PodcastGrid.stories.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { nanoid } from '@reduxjs/toolkit';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { PodcastGrid } from './PodcastGrid';
+
+export default {
+  title: 'Features/Podcasts/PodcastGrid',
+  component: PodcastGrid,
+} as ComponentMeta<typeof PodcastGrid>;
+
+const PodcastGridTemplate: ComponentStory<typeof PodcastGrid> = (props) => (
+  <PodcastGrid {...props} />
+);
+
+export const Default = PodcastGridTemplate.bind({});
+
+Default.args = {
+  podcasts: [
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+    {
+      podcastId: nanoid(),
+      coverUrl: 'https://via.placeholder.com/170x170',
+      title: 'Podcast title',
+      author: 'Podcast author',
+    },
+  ],
+};
---
+++ src/features/podcasts/PodcastGrid/PodcastGrid.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import { PodcastCard } from '../PodcastCard/PodcastCard';
+import { GridContainer } from './PodcastGrid.styles';
+
+interface PodcastProps {
+  podcasts: React.ComponentProps<typeof PodcastCard>[];
+}
+
+export const PodcastGrid: FC<PodcastProps> = ({ podcasts }) => (
+  <GridContainer>
+    {podcasts.map(({ podcastId, coverUrl, title, author }) => (
+      <PodcastCard
+        key={podcastId}
+        podcastId={podcastId}
+        coverUrl={coverUrl}
+        title={title}
+        author={author}
+      />
+    ))}
+  </GridContainer>
+);
