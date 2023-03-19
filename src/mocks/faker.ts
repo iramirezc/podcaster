99754a36d5725c49779aa1c4f3e3d5defac26125
Isaac Ramirez <iramirezc@live.com.mx>
2023-03-19 10:41:43 +0100

    Create faker.

---
+++ src/mocks/faker.ts
@@ -0,0 +1,38 @@
+import { nanoid } from '@reduxjs/toolkit';
+
+export const createFakePodcastData = (podcastId = nanoid()) => ({
+  id: {
+    attributes: {
+      'im:id': podcastId,
+    },
+  },
+  'im:artist': {
+    label: `Artist of podcast ${podcastId}`,
+  },
+  'im:image': [
+    { label: 'https://via.placeholder.com/55x55' },
+    { label: 'https://via.placeholder.com/60x60' },
+    {
+      label: 'https://via.placeholder.com/170x170',
+    },
+  ],
+  title: {
+    label: `Podcast title ${podcastId}`,
+  },
+});
+
+export const createFakePodcastsResponse = (nPodcasts: number) => {
+  const fakePodcasts: ReturnType<typeof createFakePodcastData>[] = [];
+
+  for (let i = 0; i < nPodcasts; i++) {
+    fakePodcasts.push(createFakePodcastData());
+  }
+
+  return {
+    contents: JSON.stringify({
+      feed: {
+        entry: fakePodcasts,
+      },
+    }),
+  };
+};
