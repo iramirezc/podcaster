18712b692373440b5790f34ebb118eac2588e841
Isaac Ramirez <iramirezc@live.com.mx>
2023-03-21 02:13:27 +0100

    Refactor App with a routing Layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from 'app/router';
-import { useAppSelector } from 'app/hooks';
-import { AppHeader, AppName } from 'components';
-import { selectStatus } from 'features/podcasts';
-import { StyledSpinner } from './App.styles';
 
 function App() {
-  const podcastsStatus = useAppSelector(selectStatus);
-
-  return (
-    <>
-      <AppHeader>
-        <AppName />
-        {podcastsStatus === 'loading' && <StyledSpinner />}
-      </AppHeader>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
